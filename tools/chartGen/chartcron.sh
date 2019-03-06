7deedd73c537f085f74f15f7333666dca1048f9f
zhouy7x-in-vox <zhouy7x@gmail.com>
2019-03-06 16:41:05 +0800

    ignore *.png & *.html

--- tools/chartGen/chartcron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export BASEDIR=/home/benchmark
-export HOME=$BASEDIR
-export PATH=$BASEDIR/node/bin:$PATH
-cd $BASEDIR/benchmarking/tools/chartGen/
-sh gencharts.sh
-rm -rf $BASEDIR/benchmarking/www/charts/*
-cp *.png $BASEDIR/benchmarking/www/charts

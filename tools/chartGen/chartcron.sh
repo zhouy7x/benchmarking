d6f1095cae3dbbe66a4a9753bdb64757fe4c7b6e
zhouy7x-in-lhj <zhouy7x@gmail.com>
2019-03-06 16:21:56 +0800

    add node-dc-eis and ssr to benchmarks.

--- tools/chartGen/chartcron.sh
+++ tools/chartGen/chartcron.sh
@@ -4,4 +4,5 @@ export HOME=$BASEDIR
 export PATH=$BASEDIR/node/bin:$PATH
 cd $BASEDIR/benchmarking/tools/chartGen/
 sh gencharts.sh
+rm -rf $BASEDIR/benchmarking/www/charts/*
 cp *.png $BASEDIR/benchmarking/www/charts
